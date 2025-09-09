ef6dcf30918be0a919b8027e14ccafee7321b32e
Muhammad_Farel <mhmmdfrlfrl@gmail.com>
2025-09-09 17:38:52 +0800

    Day2.java

---
+++ Day2.java
@@ -0,0 +1,7 @@
+public class ngoding2 {
+     public static void main (String [] args){
+          System.out.print("100DaysCoding");
+          System.out.println("Day2");
+          System.out.printf("Nama : %s , Umur : %d , NIM : %s " , "Muh Farel" , 18 , "D0225322" );
+     }
+}

29934eae71864b8d5c7cc25d9411bc2f27820148
Muhammad_Farel <mhmmdfrlfrl@gmail.com>
2025-09-10 16:02:13 +0800

    Day3.java

---
+++ Day3.java
@@ -0,0 +1,6 @@
+public class ngoding3 {
+     public static void main (String [] args){
+          // komentar ini hanya untuk 1 baris saja
+          /* komentar ini digunakan untuk banyak baris */
+     }
+}

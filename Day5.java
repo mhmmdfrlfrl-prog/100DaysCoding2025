5fc9bc99d3c9db5f797aaeb32e6e8e2231a8f1ae
Muhammad_Farel <mhmmdfrlfrl@gmail.com>
2025-09-12 07:30:26 +0800

    Day5.java

---
+++ Day5.java
@@ -0,0 +1,13 @@
+public class Main {
+	public static void main(String[] args) {
+		int jumlahKelasb = 28;
+		byte jumlahGabungan = 60;
+		short jumlahMabateknik = 400;
+		long jumlahMaba25 = 3000;
+		
+		System.out.println("Jumlah Kelas B \t\t: " + jumlahKelasb );
+		System.out.println("Jumlah Kelas Gabungan \t: " + jumlahGabungan);
+		System.out.println("Jumlah Maba Teknik \t: "+ jumlahMabateknik);
+		System.out.println("Jumlah Maba 2025 \t: " + jumlahMaba25);
+	}
+}

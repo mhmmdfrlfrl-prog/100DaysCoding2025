4fa1e466ba55938e4806c55d62408a341d057044
Muhammad_Farel <mhmmdfrlfrl@gmail.com>
2025-09-11 11:50:42 +0800

    Day4.java

---
+++ Day4.java
@@ -0,0 +1,17 @@
+public class Main {
+	public static void main(String[] args) {
+	    // \n untuk membuat paragraf baru
+	    System.out.println("Farel \nInformatika");
+	    // \t untuk tab
+	    System.out.println("Farel\tInformatika");
+	    // \b untuk backpase atau menghapus satu huruf ke belakang 
+	    System.out.println("Fa\brel Informatika");
+	    // \' untuk memberikan petik 1
+	    System.out.println("Farel \'Informatika\'");
+	    // \" untuk memberi petik 2
+	    System.out.println("Farel \"Informatika\"");
+	    // \\ untuk membuat atau menampilkan backslash
+	    System.out.println("Farel\\Informatika");
+	    
+	} 
+}

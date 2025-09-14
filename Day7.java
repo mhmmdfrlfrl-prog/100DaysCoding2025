7086be724ebbbc7265c53cc567305f26728c3c3b
Muhammad_Farel <mhmmdfrlfrl@gmail.com>
2025-09-14 16:54:50 +0800

    Day7.java

---
+++ Day7.java
@@ -0,0 +1,11 @@
+public class Main {
+	public static void main(String[] args) {
+	   // '7' adalah karakter (char)
+	   // true dan false adalah Boolean (boolean)
+			char a = '7';  
+			boolean b = true;
+			
+			System.out.println("Sekarang Adalah Hari Ke : " + a );
+			System.out.printf("7 =  "+ b);				
+   }
+}

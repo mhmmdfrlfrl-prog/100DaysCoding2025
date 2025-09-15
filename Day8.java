928ca61b83265f918924458e9499acb17ba50bae
Muhammad_Farel <mhmmdfrlfrl@gmail.com>
2025-09-15 11:15:26 +0800

    Day8.java

---
+++ Day8.java
@@ -0,0 +1,7 @@
+public class Main {
+	public static void main(String[] args) {
+		// String digunakan untuk kata atau kalimat 
+		String nama = "Muh Farel";
+		 System.out.println("Nama :" + nama);
+	}
+}

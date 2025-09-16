6f37d17a06b82307a12d0fc2233d724d40daa115
Muhammad_Farel <mhmmdfrlfrl@gmail.com>
2025-09-16 16:33:49 +0800

    Day9.java

---
+++ Day9.java
@@ -0,0 +1,11 @@
+public class Main {
+	public static void main(String[] args) {
+		// Deklarasi 
+		int a;
+	  // Inisialisasi variabel b menggunakan integer
+		int b = 8;
+		// Inisialisasi update menggunakan variabel b
+		b = 9;
+		System.out.println(b);
+	}
+}

46414178677f96bb9087962dcc8ccf8e7b1f9d43
Muhammad_Farel <mhmmdfrlfrl@gmail.com>
2025-09-17 20:12:35 +0800

    Day10.java

---
+++ Day10.java
@@ -0,0 +1,8 @@
+public class Main {
+	public static void main(String[] args) {
+       // kegunaan final yaitu tidak bisa di ubah,kalau diubah bisa eror
+	 	final String nama = "Muh Farel";
+		System.out.println("Nama :" + nama );
+		
+	}
+}

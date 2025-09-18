fc06c34277d1a1570ee3e9eb34b7731b6e98fae6
Muhammad_Farel <mhmmdfrlfrl@gmail.com>
2025-09-18 17:48:29 +0800

    Day11.java

---
+++ Day11.java
@@ -0,0 +1,15 @@
+package Day11;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner input = new Scanner (System.in);
+		System.out.println("Masukkan Nama :");
+		String nama = input.nextLine();
+		
+		System.out.println("Masukkan Umur");
+		int umur = input.nextInt ();
+		
+	}
+}

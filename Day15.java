1df23e05d075f3f4c0a7ac70208f000344c0b559
Muhammad_Farel <mhmmdfrlfrl@gmail.com>
2025-09-22 00:06:45 +0800

    Day15.java

---
+++ Day15.java
@@ -0,0 +1,13 @@
+
+public class ngoding {
+    
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 20;
+        int c = a+b;
+        int d = a-b;
+        System.out.println(c);
+        System.out.println(d);
+    }
+    
+}

038bf717066a8c48b46c858263c6100eb20ebe7c
Muhammad_Farel <mhmmdfrlfrl@gmail.com>
2025-09-23 20:05:22 +0800

    Day16.java

---
+++ Day16.java
@@ -0,0 +1,23 @@
+
+public class main16 {
+    
+    public static void main(String[] args) {
+        int angka1 = 20;
+        int angka2 = 5;
+        int  hasil1 = angka1*angka2;
+        int hasil2 = angka1/angka2;
+        
+        
+        System.out.println(hasil1);
+        System.out.println(hasil2);
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+}

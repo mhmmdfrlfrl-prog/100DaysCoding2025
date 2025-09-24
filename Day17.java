7daea7165e9994983b0f9e90762a05c21f96c088
Muhammad_Farel <mhmmdfrlfrl@gmail.com>
2025-09-24 20:54:21 +0800

    Day17.java

---
+++ Day17.java
@@ -0,0 +1,18 @@
+
+public class CodinganDay17 {
+    
+    public static void main(String[] args) {
+        int angka1 = 10;
+        int angka2 = 6;
+        int sisaBagi = angka1%angka2;
+        
+        System.out.println(sisaBagi);
+        
+        
+        
+        
+        
+        
+    }
+    
+}

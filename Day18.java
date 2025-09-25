b2b0170800bade82f3d7cd2dbcb859320e31b203
Muhammad_Farel <mhmmdfrlfrl@gmail.com>
2025-09-25 20:49:01 +0800

    Day18.java

---
+++ Day18.java
@@ -0,0 +1,27 @@
+public class Class111 {
+    
+    public static void main(String[] args) {
+        byte angka = 50;
+        short angka1 = angka;
+        int angka2 = angka1;
+        long angka3 = angka2;
+        float angka4 = angka3;
+        double angka5 = angka4;
+        
+        
+        System.out.println("byte :" + angka);
+        System.out.println("short:" + angka1);
+        System.out.println("int :" + angka2);
+        System.out.println("long:" + angka3);
+        System.out.println("float:" + angka4);
+        System.out.println("double :" + angka5);
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+}

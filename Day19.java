859c58a0e7c2c4067be846a55c52ce2f1c049bb5
Muhammad_Farel <mhmmdfrlfrl@gmail.com>
2025-09-26 21:24:24 +0800

    Day19.java

---
+++ Day19.java
@@ -0,0 +1,29 @@
+public class Coding19 {
+    
+    public static void main(String[] args) {
+        
+        double angkaDouble = 25.52;
+        float angkaFloat = (float) angkaDouble;
+        long angkaLong = (long) angkaFloat;
+        int angkaInt = (int) angkaLong;
+        short angkaShort = (short) angkaInt;
+        byte angkaByte = (byte) angkaShort;
+        
+        
+        System.out.println(angkaDouble);
+        System.out.println(angkaFloat);
+        System.out.println(angkaLong);
+        System.out.println(angkaInt);
+        System.out.println(angkaShort);
+        System.out.println(angkaByte);
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+}

e560609aab82703bd3a6c7efbd219248f5138ab0
Muhammad_Farel <mhmmdfrlfrl@gmail.com>
2025-09-27 21:47:40 +0800

    Day20.java

---
+++ Day20.java
@@ -0,0 +1,25 @@
+
+public class Day20 {
+    
+    public static void main(String[] args) {
+        int angka = 99;
+        String angkaString = String.valueOf(angka);
+        System.out.println(angkaString);
+        
+        
+        int jumlah = 66;
+        String jumlahString = jumlah + "";
+        System.out.println(jumlahString);
+        
+        
+        double berat = 55.25;
+        String beratString = Double.toString(berat);
+        System.out.println(beratString);
+        
+        
+        
+        
+        
+    }
+    
+}

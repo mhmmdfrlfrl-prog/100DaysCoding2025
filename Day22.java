783056c2ee8924df4cf72773648dcb43fbd96a51
Muhammad_Farel <mhmmdfrlfrl@gmail.com>
2025-09-29 22:36:27 +0800

    Day22.java

---
+++ Day22.java
@@ -0,0 +1,16 @@
+
+public class Day22 {
+    
+    public static void main(String[] args) {
+        int sisi = 6;
+        
+        int Luas = sisi*sisi;
+        System.out.println("Luas persegi =" + Luas + " cm²");
+        
+        
+        
+        
+        
+    }
+    
+}

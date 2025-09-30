b4b940b2e4d1ecce0d2314823882743abd81d148
Muhammad_Farel <mhmmdfrlfrl@gmail.com>
2025-09-30 22:15:35 +0800

    Day23.java

---
+++ Day23.java
@@ -0,0 +1,15 @@
+
+public class Day23 {
+    
+    public static void main(String[] args) {
+        int panjang = 5;
+        int lebar = 7;
+        
+        int luas = panjang*lebar;
+        
+        
+        System.out.println("Luas = " + luas);
+        
+    }
+    
+}

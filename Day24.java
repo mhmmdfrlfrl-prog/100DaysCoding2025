9182e70244d5179500b728c362435edd38d62aac
Muhammad_Farel <mhmmdfrlfrl@gmail.com>
2025-10-01 20:06:49 +0800

    Day24.java

---
+++ Day24.java
@@ -0,0 +1,17 @@
+
+public class Day24 {
+    
+    public static void main(String[] args) {
+        
+        double jarijari= 6;
+        double pi = 3.14;
+        
+        double luas = pi *jarijari*jarijari;
+        
+        System.out.println("Luas lingkaran jari-jari = " + jarijari + "\nadalah "+ luas);
+        
+        
+        
+    }
+    
+}

e4e9b2ebb9a21ada2b7b979d35ef522010f76fb8
Muhammad_Farel <mhmmdfrlfrl@gmail.com>
2025-10-02 23:02:54 +0800

    Day25.java

---
+++ Day25.java
@@ -0,0 +1,17 @@
+
+public class Day25 {
+    
+    public static void main(String[] args) {
+        int a = 9;
+        a++;
+        System.out.println(a);
+        int b = 8;
+        b--;
+        System.out.println(b);
+        
+        
+        
+        
+    }
+    
+}

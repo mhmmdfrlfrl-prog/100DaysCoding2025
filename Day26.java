a512a1fe31ef3b081c25c3d00e447d1ca7d8edf0
Muhammad_Farel <mhmmdfrlfrl@gmail.com>
2025-10-03 22:41:31 +0800

    Day26.java

---
+++ Day26.java
@@ -0,0 +1,25 @@
+
+public class Day26 {
+    
+    public static void main(String[] args) {
+        int a = 10;
+        System.out.println(a);
+        a += 5;
+        System.out.println(a);
+        a-= 7;
+        System.out.println(a);
+        a *= 2;
+        System.out.println(a);
+        a /= 4;
+        System.out.println(a);
+        a %= 2;
+        System.out.println(a);
+        
+        
+        
+        
+        
+        
+    }
+    
+}

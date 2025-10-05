df5e538e6dc534c203fe6e6a7c41fafbf03a66ef
Muhammad_Farel <mhmmdfrlfrl@gmail.com>
2025-10-05 21:25:36 +0800

    Day28.java

---
+++ Day28.java
@@ -0,0 +1,13 @@
+
+public class Day28 {
+    
+    public static void main(String[] args) {
+        int a = 18;
+        int b = 18;
+        
+        System.out.println(a==b);
+        System.out.println(a!=b);
+        
+    }
+    
+}

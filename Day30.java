4d6fce036817d318353dd2643241758661f6c77f
Muhammad_Farel <mhmmdfrlfrl@gmail.com>
2025-10-07 21:39:11 +0800

    Day30.java

---
+++ Day30.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Day30 {
+    
+    public static void main(String[] args) {
+        Scanner input = new Scanner (System.in);
+        
+        System.out.println("masukkan angka A ");
+        int A = input.nextInt();
+        System.out.println("masukkan angka B ");
+        int B = input.nextInt();
+        
+        System.out.println(A<=B);
+        
+        System.out.println(A>=B);
+        
+        
+        
+        
+    }
+    
+}

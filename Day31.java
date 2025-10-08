fef78eb08b14bd14a02e40c2df690d0a899a8c85
Muhammad_Farel <mhmmdfrlfrl@gmail.com>
2025-10-08 20:52:41 +0800

    Day31.java

---
+++ Day31.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Day31 {
+    
+    public static void main(String[] args) {
+        Scanner input = new Scanner (System.in);
+        
+        System.out.println("masukkan angka A ");
+        int A = input.nextInt();
+        System.out.println("masukkan angka B ");
+        int B = input.nextInt();
+        
+        System.out.println(A<=B && A<=B);
+        
+        
+        
+        
+        
+    }
+    
+}

a07d27f60d0b7f440c61f846b2f783f523c09df7
Muhammad_Farel <mhmmdfrlfrl@gmail.com>
2025-10-10 21:13:25 +0800

    Day33.java

--- Day33.java
+++ Day33.java
@@ -1,16 +1,12 @@
 
-import java.util.Scanner;
+
 public class Day33 {
     
     public static void main(String[] args) {
-        Scanner input = new Scanner (System.in);
+        int a = 5;
+        int b = 6;    
         
-        System.out.println("masukkan angka A ");
-        int A = input.nextInt();
-        System.out.println("masukkan angka B ");
-        int B = input.nextInt();
-        
-        System.out.println(A<=B != A<=B);
+        System.out.println(!(a<b));
         
     }
 }

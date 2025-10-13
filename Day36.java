240db124ec39e5f6b0a4c69f074de6edeb7a4cb2
Muhammad_Farel <mhmmdfrlfrl@gmail.com>
2025-10-13 21:54:10 +0800

    Day36.java

---
+++ Day36.java
@@ -0,0 +1,24 @@
+
+public class Day036 {
+    
+    public static void main(String[] args) {
+        int uts = 80;
+        
+        if(uts>=75){
+        System.out.println("lulus");
+           if (uts>=90){
+               System.out.println("dengan nilai A ");
+           } else if (uts>=80){
+               System.out.println("dengan nilai b");
+           } else {
+               System.out.println("dengan nilai c");
+               
+           }
+    } else {
+        System.out.println("tudak lulus");
+        
+    } 
+        
+  }
+    
+}

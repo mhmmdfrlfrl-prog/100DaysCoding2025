5d728c87c5621b99f0d791f0558fa1e4606d83a3
Muhammad_Farel <mhmmdfrlfrl@gmail.com>
2025-10-15 21:32:09 +0800

    Day38.java

---
+++ Day38.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Day38 {
+    
+    public static void main(String[] args) {
+        Scanner input = new Scanner (System.in);
+        System.out.println("Masukkan Angka ");
+        int angka = input.nextInt();
+        
+        if(angka>0){
+            System.out.println(angka+" adalah bilangan positif");
+        } else if(angka<0){
+            System.out.println(angka+" adalah bilangan negatif");
+        } else {
+            System.out.println(angka+" adalah nol");
+        }
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+}

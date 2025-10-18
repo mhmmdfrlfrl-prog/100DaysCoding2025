8cb3e7fca42952f2bb80f2da060e6464f7a4a4ff
Muhammad_Farel <mhmmdfrlfrl@gmail.com>
2025-10-18 22:16:37 +0800

    Day41.java

---
+++ Day41.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class Day41 {
+    
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println("Masukkan panjang taman: ");
+        double panjang = input.nextDouble();
+        System.out.println("Masukkan lebar taman: ");
+        double lebar = input.nextDouble();
+        
+        double luas = panjang*lebar;
+        
+        System.out.printf("Luas taman yang akan ditanami rumput adalah %.2f meter persegi ",luas );
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+}

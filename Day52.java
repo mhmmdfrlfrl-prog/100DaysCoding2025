489713b95eb8ef95e8d699c6ae92362ddc038a50
Muhammad_Farel <mhmmdfrlfrl@gmail.com>
2025-10-29 22:35:27 +0800

    Day52.java

---
+++ Day52.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Day52 {
+    
+    public static void main(String[] args) {
+        Scanner input = new Scanner (System.in);
+        
+        System.out.println("Masukkan Angka ");
+        int a = input.nextInt();
+        
+        while (a<=5){
+            System.out.print(a+" ");
+            a++;
+        }
+        
+        
+    }
+    
+}

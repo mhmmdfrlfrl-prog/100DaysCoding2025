8fbc3e08ea81fc8a655d9b3e34f1307b4cded589
Muhammad_Farel <mhmmdfrlfrl@gmail.com>
2025-11-02 22:54:42 +0800

    Day56.java

---
+++ Day56.java
@@ -0,0 +1,19 @@
+
+package com.mycompany.mavenproject1;
+
+
+
+
+public class Day56 {
+    public static void main(String[] args) {
+        
+        
+       int a = 1;
+        do{
+            System.out.print(a+" ");
+            a++;
+                   
+        } while(a<=5);
+    }
+}
+

e3267ea6661477b4de1656af2bde8cad64e8ebd9
Muhammad_Farel <mhmmdfrlfrl@gmail.com>
2025-11-13 22:55:35 +0800

    Day67.java

---
+++ Day67.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class ngoding67 {
+    
+    public static void main(String[] args) {
+        Scanner input = new Scanner (System.in);
+        
+        System.out.println("Masukkan N:");
+        int n = input.nextInt();
+        
+        int total = 1;
+        
+        for(int i = 1; i<=n; i++){
+            
+            System.out.print("X");
+        }
+        
+    }
+    
+}

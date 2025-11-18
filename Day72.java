e9760de85c8bf130d10d68ae8279bbedbf13908b
Muhammad_Farel <mhmmdfrlfrl@gmail.com>
2025-11-18 22:50:24 +0800

    Day72.java

---
+++ Day72.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Day72 {
+    
+    public static void main(String[] args) {
+        Scanner input = new Scanner (System.in);
+        
+        System.out.print("Masukkan N :");
+        int n = input.nextInt();
+        
+        for(int i = n; i>=1; i--){
+            for(int j = 1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        
+    }
+    
+}

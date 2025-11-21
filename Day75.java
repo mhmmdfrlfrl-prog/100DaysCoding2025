8441ace072ae5806453890106c17b388c8b19166
Muhammad_Farel <mhmmdfrlfrl@gmail.com>
2025-11-21 21:58:36 +0800

    Day75.java

---
+++ Day75.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class ngoding75 {
+    
+    public static void main(String[] args) {
+        Scanner input = new Scanner (System.in);
+        
+        System.out.println("Masukkan N:");
+        String n = input.nextLine();
+        
+        
+        System.out.println("length :"+n.length());
+        System.out.println("toUpperCase:"+n.toUpperCase());
+        System.out.println("toLowerCase:"+n.toLowerCase());
+    }
+    
+}

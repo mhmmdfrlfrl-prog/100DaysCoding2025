26234cda62191d99ee72518919a0e90cc5394146
Muhammad_Farel <mhmmdfrlfrl@gmail.com>
2025-11-26 22:00:52 +0800

    Day80.java

---
+++ Day80.java
@@ -0,0 +1,12 @@
+
+public class Day80 {
+    
+    public static void main(String[] args) {
+        int [] angka = {5,10,15,20};
+        
+        for(int i = 0;i< angka.length;i++){
+            System.out.println(angka[i]);
+        }
+    }
+    
+}

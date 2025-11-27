6573971695cc701d4a74415bf967c1d867641273
Muhammad_Farel <mhmmdfrlfrl@gmail.com>
2025-11-27 20:28:16 +0800

    Day81.java

---
+++ Day81.java
@@ -0,0 +1,15 @@
+
+public class Day81 {
+    
+    public static void main(String[] args) {
+        
+        String [] nama = {"Budi", "Adi", "Putra", "Asep"};
+        
+        int [] angka = {5, 10, 15, 20};
+        
+        System.out.println(nama[2]);
+        System.out.println(angka[3]);
+        
+    }
+    
+}

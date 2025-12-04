cf429bab00440d57bb6cadac029441824735d7cb
Muhammad_Farel <mhmmdfrlfrl@gmail.com>
2025-12-04 20:58:51 +0800

    Day88.java

---
+++ Day88.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javaapplication13;
+
+
+
+/**
+ *
+ * @author user
+ */
+public class Day88 {
+    public static void main(String[] args) {
+        
+        String [] nama = {"Farel","Albar","Yogi","Putra"};
+        
+        
+        for(String a : nama){
+            System.out.print(a+",");
+            
+        }
+    }
+}

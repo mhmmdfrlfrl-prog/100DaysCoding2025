d9c98061ba498f88aa1b69f865bad3ae8201be5b
Muhammad_Farel <mhmmdfrlfrl@gmail.com>
2025-12-05 23:23:56 +0800

    Day89.java

---
+++ Day89.java
@@ -0,0 +1,17 @@
+package Day89;
+
+/**
+ *
+ * @author user
+ */
+public class java {
+    public static void kata() {
+        System.out.println("Imposibble is nothing");
+    }
+    
+    
+    
+    public static void main(String[] args) {
+        kata();
+    }
+}

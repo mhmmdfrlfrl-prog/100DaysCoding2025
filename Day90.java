6d54913a403d11134a8b521be633f053d78bf07a
Muhammad_Farel <mhmmdfrlfrl@gmail.com>
2025-12-06 22:06:59 +0800

    Day90.java

---
+++ Day90.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Dayy90;
+
+/**
+ *
+ * @author user
+ */
+public class Day90 {
+    public static void main(String[] args) {
+        System.out.println(kelilingPersegi(3,2));
+    }
+    public static int kelilingPersegi(int p,int l){
+        return 2 * (p+l);
+    }
+}

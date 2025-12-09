89e22a2887871b1d7f1c3b8d1e36e70addccfa00
Muhammad_Farel <mhmmdfrlfrl@gmail.com>
2025-12-09 23:31:33 +0800

    Day93.java

---
+++ Day93.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package farel;
+
+/**
+ *
+ * @author user
+ */
+public class Day93 {
+    public static void main(String[] args) {
+        double angka = 35.5;
+        
+        System.out.println(Math.ceil(angka));
+         System.out.println(Math.floor(angka));
+          System.out.println(Math.round(angka));
+    }
+}

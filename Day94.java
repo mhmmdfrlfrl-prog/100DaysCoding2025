1e5eff225db494c534e4c77eff42489b8a2fe36c
Muhammad_Farel <mhmmdfrlfrl@gmail.com>
2025-12-10 18:01:39 +0800

    Day94.java

---
+++ Day94.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package farel;
+
+/**
+ *
+ * @author user
+ */
+public class Day94 {
+    public static void main(String[] args) {
+        kata();
+    }
+    
+    public static void kata(){
+        System.out.println("Bismillah Bisaa:)");
+    }
+}
